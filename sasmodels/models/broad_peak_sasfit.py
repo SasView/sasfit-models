119daa8e62efc483e1a7508068ca7e8cbf2040ec
Wojciech Potrzebowski <Wojciech.Potrzebowski@esss.se>
2016-04-28 11:47:08 +0200

    Conversion script for SASFit

---
+++ sasmodels/models/broad_peak_sasfit.py
@@ -0,0 +1,107 @@
+r"""
+This model calculates an empirical functional form for SAS data characterized
+by a broad scattering peak. Many SAS spectra are characterized by a broad peak
+even though they are from amorphous soft materials. For example, soft systems
+that show a SAS peak include copolymers, polyelectrolytes, multiphase systems,
+layered structures, etc.
+
+The d-spacing corresponding to the broad peak is a characteristic distance
+between the scattering inhomogeneities (such as in lamellar, cylindrical, or
+spherical morphologies, or for bicontinuous structures).
+
+Definition
+----------
+
+The scattering intensity $I(q)$ is calculated as
+
+.. math::
+
+    I(q) = \frac{A}{q^n} + \frac{C}{1 + (|q - q_0|\xi)^m} + B
+
+Here the peak position is related to the d-spacing as $q_o = 2\pi / d_o$.
+
+$A$ is the Porod law scale factor, $n$ the Porod exponent, $C$ is the Lorentzian 
+scale factor, $m$ the exponent of q, \ |xi|\  the screening length, and $B$ the flat background.
+
+For 2D data the scattering intensity is calculated in the same way as 1D,
+where the $q$ vector is defined as
+
+.. math::
+
+    q = \sqrt{q_x^2 + q_y^2}
+
+
+References
+----------
+
+None.
+
+*2013/09/09 - Description reviewed by King, S and Parker, P.*
+
+"""
+
+from numpy import inf, sqrt
+
+name = "broad_peak"
+title = "Broad Lorentzian type peak on top of a power law decay"
+description = """\
+      I(q) = scale_p/pow(q,exponent)+scale_l/
+      (1.0 + pow((fabs(q-q_peak)*length_l),exponent_l) )+ background
+
+      List of default parameters:
+      porod_scale = Porod term scaling
+      porod_exp = Porod exponent
+      lorentz_scale = Lorentzian term scaling
+      lorentz_length = Lorentzian screening length [A]
+      peak_pos = peak location [1/A]
+      lorentz_exp = Lorentzian exponent
+      background = Incoherent background"""
+category = "shape-independent"
+
+# pylint: disable=bad-whitespace, line-too-long
+#             ["name", "units", default, [lower, upper], "type", "description"],
+parameters = [["porod_scale",    "",  1.0e-05, [-inf, inf], "", "Power law scale factor"],
+              ["porod_exp",      "",      3.0, [-inf, inf], "", "Exponent of power law"],
+              ["lorentz_scale",  "",     10.0, [-inf, inf], "", "Scale factor for broad Lorentzian peak"],
+              ["lorentz_length", "Ang",  50.0, [-inf, inf], "", "Lorentzian screening length"],
+              ["peak_pos",       "1/Ang", 0.1, [-inf, inf], "", "Peak position in q"],
+              ["lorentz_exp",    "",      2.0, [-inf, inf], "", "Exponent of Lorentz function"],
+             ]
+# pylint: enable=bad-whitespace, line-too-long
+
+def Iq(q,
+       porod_scale=1.0e-5,
+       porod_exp=3.0,
+       lorentz_scale=10.0,
+       lorentz_length=50.0,
+       peak_pos=0.1,
+       lorentz_exp=2.0):
+    """
+    :param q:              Input q-value
+    :param porod_scale:    Power law scale factor
+    :param porod_exp:      Exponent of power law
+    :param lorentz_scale:  Scale factor for broad Lorentzian peak
+    :param lorentz_length: Lorentzian screening length
+    :param peak_pos:       Peak position in q
+    :param lorentz_exp:    Exponent of Lorentz function
+    :return:               Calculated intensity
+    """
+
+    inten = (porod_scale / q ** porod_exp + lorentz_scale
+             / (1.0 + (abs(q - peak_pos) * lorentz_length) ** lorentz_exp))
+    return inten
+Iq.vectorized = True  # Iq accepts an array of q values
+
+def Iqxy(qx, qy, *args):
+    """
+    :param qx:   Input q_x-value
+    :param qy:   Input q_y-value
+    :param args: Remaining arguments
+    :return:     2D-Intensity
+    """
+    return Iq(sqrt(qx ** 2 + qy ** 2), *args)
+Iqxy.vectorized = True # Iqxy accepts an array of qx, qy values
+
+demo = dict(scale=1, background=0,
+            porod_scale=1.0e-05, porod_exp=3,
+            lorentz_scale=10, lorentz_length=50, peak_pos=0.1, lorentz_exp=2)
---
+++ sasmodels/sasfit_convert.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Program to convert sasfit form factor files to sasmodel template
+
+In principle the template should provide already working model
+but in some cases some information may need to be filled in manually
+
+The script will perform following:
+1) Remove sasfit specific includes
+2) Translate sasfit parameters to sasmodels parameter table
+3) Convert sasfit function headers to Iq, Iqxy
+4) Translates sasfit errors handling into parameter ranges
+
+The script returns converted c file and python template file.
+The template file should in principle be ready to use but
+some additional information ,may need to be filled in
+"""
+#TODO: Get the default values for the model parameters
+#TODO: Retrive parameter description automatically
+#TODO: Add categroy as an input parameter?
+#TODO: Units
+#TODO: Form volume (and specify volume params)
+
+__author__ = "Wojtek Potrzebowski"
+__maintainer__ = "Wojtek Potrzebowski"
+__email__ = "Wojciech.Potrzebowski@esss.se"
+
+import optparse
+from re import search
+from string import replace
+
+exclude_list = ["#include", "#define",
+                "sasfit_get_param",
+                "SASFIT_ASSERT_PTR", "SASFIT_CHECK_COND1",
+                "SASFIT_CHECK_COND", "SASFIT_CHECK_SUB_ERR"]
+
+substitution_dict = {"scalar":"double"}
+
+def extract_parameters_definition( model_name, tcl_filename ):
+    """
+    Extracting the definitions of paramters that can be then supplied with the
+    parameter table
+
+    :param tcl_file:
+    :return:
+    """
+    tcl_lines = open(tcl_filename).readlines()
+    parameters_definition = []
+    for index, line in enumerate(tcl_lines):
+        if search(model_name+ " {", line) or search("\""+model_name+"\" {", line):
+            model_def_index = index
+    model_def_lines = tcl_lines[model_def_index+2:model_def_index+13]
+    for def_line in model_def_lines:
+        pardef = def_line.split("return \"")[1].strip("\\n\"}\r\n")
+        parameters_definition.append(pardef)
+    return parameters_definition
+
+def generate_python_header(model_name, output_python_file, parameters_definition):
+    """
+    Python file header generator
+    :param output_python_file:
+    :return:
+    """
+    header_lines = "r\"\"\"\n"
+
+    header_lines += "This file has been automatically gereated by sasfit_convert\n"
+
+    header_lines += "The model calculates an empirical functional form " \
+                    "for SAS data characterized\n"
+    header_lines += "by "+model_name+"\n\n"
+    header_lines += "Definition:\n"
+    header_lines += "-----------\n\n"
+
+    header_lines += "References:\n"
+    header_lines += "-----------\n\n"
+
+    header_lines += "\"\"\"\n"
+
+    header_lines += "from numpy import inf\n\n"
+
+    header_lines += "name = \""+model_name+"\"\n"
+    header_lines += "title = \" \"\n"
+    header_lines += "description = \""+parameters_definition[0]+"\"\n"
+    header_lines += "category = \" \"\n"
+
+    output_python_file.writelines(header_lines)
+
+def convert_sasfit_model(model_name, sasfit_file, output_c_file,
+                         output_python_file, parameters_definition):
+    """
+    Main conversion function
+    :param sasfit_file:
+    :param output_c_file:
+    :param output_python_file:
+    :return:
+    """
+    sasfit_lines = open(sasfit_file).readlines()
+    output_c_lines = []
+    parameters = []
+    output_python_lines = []
+    #DO regular expression and remove if it is in line
+    c_intro_lines = "///////////////////////////////////////////////////\n"
+    c_intro_lines += "//    This is automatically genearted file       //\n"
+    c_intro_lines += "//    by sasfit_convert.py                       //\n"
+    c_intro_lines += "//    Some editting might be required            //\n"
+    c_intro_lines += "///////////////////////////////////////////////////\n\n"
+
+    output_c_lines.append(c_intro_lines)
+
+    #There a few different ways to extract parameters
+    #1. From define statement in the c file
+    for line in sasfit_lines:
+        #TODO: There are other define statements, so check if it is not failing
+        regm = search("#define", line)
+        if regm:
+            parameters.append(line[regm.end()+1:].split("\t")[0])
+    #2. From sasfit_get_param
+    if len(parameters) == 0:
+        for line in sasfit_lines:
+            regm = search("sasfit_get_param\(", line)
+            if regm:
+                params = line[regm.end():].strip(");\r\n").split(", ")
+                number_of_params = int(params[1])
+                parameters = map(lambda p : p.lstrip("&"),params[2:2+number_of_params])
+
+    #But if 2 above don't work then read it from parameters definition
+    if len(parameters) == 0:
+        for pardef in parameters_definition[1:]:
+            param = pardef.split(":")[0]
+            if param !="":
+                parameters.append(param)
+
+    #And if this still doesn't work stop computation
+    if len(parameters) == 0:
+        exit()
+    #Add paramters to excluded list, so they don't get redifned - shaky
+    exclude_list.append("scalar "+", ".join(parameters))
+
+    header_Iq_line = "double Iq( double q,"
+    for line in sasfit_lines:
+        line = line.strip()
+        allowed = 1
+
+        #Create a new header for Iq function
+        if search("sasfit_ff_"+model_name, line) and not search("src", line):
+            for param in parameters[:-1]:
+                header_Iq_line+="\n\tdouble "+param+","
+            header_Iq_line+="\n\tdouble "+parameters[-1]+")\n"
+            output_c_lines.append(header_Iq_line)
+            allowed = 0
+
+        for banned_term in exclude_list:
+            if search(banned_term, line):
+                allowed = 0
+
+        #Replace string in line when need
+        for sub in substitution_dict.keys():
+            if search(sub, line):
+                line = line.replace(sub, substitution_dict[sub])
+
+        #Skip empty lines
+        if line=="":
+            allowed = 0
+
+        if allowed:
+            output_c_lines.append(line+"\n")
+
+    #TODO: Not exactly sure how to handle this
+    header_Iqxy_line = "double Iqxy( double qx, double qy,"
+    for param in parameters[:-1]:
+        header_Iqxy_line+="\n\tdouble "+param+","
+    header_Iqxy_line+="\n\tdouble "+parameters[-1]+")\n"
+    header_Iqxy_line+="{\n"
+    header_Iqxy_line+="\tdouble q = sqrt(qx*qx + qy*qy);\n"
+    header_Iqxy_line+="\treturn Iq( q,"
+    for param in parameters[:-1]:
+        header_Iqxy_line+=" "+param+","
+    header_Iqxy_line+=" "+parameters[-1]+");\n"
+    header_Iqxy_line+="}\n"
+    output_c_lines.append(header_Iqxy_line)
+
+    #Generating python parameter file
+    #It looks from interface that first value is set to 10.0, 4th to 1.0
+    #and the rest is 0.0 and apparently standard models takes no more than
+    #10 pramateres
+    parameters_values = [10.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+    output_python_lines = "#pylint: disable=bad-whitespace, line-too-long\n"
+    output_python_lines += "parameters = [\n"
+    for index, param in enumerate(parameters):
+        output_python_lines += " [ \""+param+"\", \t\"\", \t"\
+                               +str(parameters_values[index])+", " \
+                                "\t[-inf, inf], \t\"\", " \
+                                "\t\""+parameters_definition[index+1]+"\"],\n"
+    output_python_lines += "]\n"
+    output_python_lines +=" #pylint: enable=bad-whitespace, line-too-long\n\n"
+    #FIXME: It should refer to c_file not model name
+    output_python_lines +="source = [\"sasfit_"+model_name+".c\" ]\n\n"
+
+    output_python_lines +="demo = dict(\n"
+    for index, param in enumerate(parameters[:-1]):
+        output_python_lines += "\t"+param+" = " \
+            +str(parameters_values[index])+",\n"
+
+    output_python_lines += "\t"+parameters[-1]+" = "\
+                           +str(parameters_values[len(parameters)-1]) + ")\n"
+
+    output_c_file.writelines(output_c_lines)
+    output_python_file.writelines(output_python_lines)
+
+if __name__=="__main__":
+    doc = """
+            Script to convert sasfit files to sasmodels
+            Usage: python sasfit_convert.py --help
+        """
+    print doc
+    usage = "usage: %prog [options] args"
+    option_parser_class = optparse.OptionParser
+    parser = option_parser_class( usage = usage, version='0.1' )
+
+    parser.add_option("-f", "--sasfit_file", dest="sasfit_file",
+                      help="SASFit c file [OBLIGATORY]")
+    parser.add_option("-o", "--output_file", dest="output_file",
+                      help="Output c file and python file name [NO EXTENSION]")
+    parser.add_option("-t", "--tcl_file", dest="tcl_file",
+                      help="TCL file containing parameter definition c file ")
+
+    options, args = parser.parse_args()
+
+
+    if options.output_file:
+        model_name = options.output_file
+    else:
+        #Remove sasfit_ff prefix and .c suffix
+        model_name = options.sasfit_file.lstrip("../sasfit_ff_").rstrip(".c")
+
+    output_c_filename = "sasfit_"+model_name+".c"
+    output_python_filename = "sasfit_"+model_name+".py"
+
+    output_c_file = open(output_c_filename,"w")
+    output_python_file =  open(output_python_filename,"w")
+
+    parameters_definition = extract_parameters_definition(model_name,
+                                                          options.tcl_file)
+
+    generate_python_header(model_name, output_python_file, parameters_definition)
+
+    convert_sasfit_model(model_name, options.sasfit_file, output_c_file,
+                         output_python_file, parameters_definition)
